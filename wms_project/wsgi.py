e717c6dcc93fae04470f6e752c3824a6f02c83ff
Jaxon Ryan <jaxonryants@Mac.lan>
2025-05-07 13:52:25 -0500

    Run migrations automatically on Render startup

--- wms_project/wsgi.py
+++ wms_project/wsgi.py
@@ -1,26 +1,13 @@
-"""
-WSGI config for wms_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wms_project.settings')
 
-# --- Run migrations automatically on startup ---
-import django
-django.setup()
-from django.core.management import call_command
-
-try:
-    call_command('migrate', interactive=False)
-except Exception as e:
-    print("Migration error during WSGI startup:", e)
-# ------------------------------------------------
-
 application = get_wsgi_application()
+
+# Automatically run migrations at startup on Render
+try:
+    from django.core.management import call_command
+    call_command("migrate", interactive=False)
+except Exception as e:
+    print(f"Migration error: {e}")
