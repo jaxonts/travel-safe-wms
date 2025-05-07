4a984bd7cc7bff2681f7655a8a384f0a98a69155
Jaxon Ryan <jaxonryants@Mac.lan>
2025-05-07 13:36:15 -0500

    Add automatic migration on WSGI startup

--- wms_project/wsgi.py
+++ wms_project/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wms_project.settings')
+
+# --- Run migrations automatically on startup ---
+import django
+django.setup()
+from django.core.management import call_command
+
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Migration error during WSGI startup:", e)
+# ------------------------------------------------
 
 application = get_wsgi_application()
